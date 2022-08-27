3c7d6406bd1dc124565e5e1320b587663426a63b
Sam <samuel.gacho@africastalking.com>
2022-08-27 19:25:26 +0300

    Add major changes

---
+++ src/components/Cart/cart-contents.vue
@@ -0,0 +1,87 @@
+<template>
+    <div class="column-parent">
+        <div class="cart-content">
+            <div class="cart-items">
+                <h3>Your Order:</h3>
+                <div class="item" v-for="item in cartItems" :key="item.no">
+                    <img class="item-image" :src="item.imgUrl" alt="product" />
+                    <div class="item-text-meta">
+                        <span>{{ item.wineNumber }}</span>
+                        <span>{{ item.wineName }}</span>
+                        <span>bottle: {{ item.bottleQuantity }}</span>
+                        <span>case: {{ item.caseQuantity }}</span>
+                    </div>
+                </div>
+            </div>
+            <div class="invoice-section">
+                <h3>Total Amount:</h3>
+                <span class="invoice-amount">Kshs. {{ this.$route.params.balance }}</span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CartContents',
+    data() {
+        return {
+            cartItems: [],
+        };
+    },
+
+    created() {
+        this.cartItems = JSON.parse(this.$route.params.cartItems);
+    },
+};
+</script>
+
+<style scoped>
+@import url("./styles.css");
+
+.cart-content {
+  margin: 1em;
+  padding: 0.5em;
+  border-radius: 7px;
+  background: var(--transparent-white);
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+}
+.item {
+  display: flex;
+  justify-content: flex-start;
+  padding: 5px;
+  border-radius: 5px;
+  border: 1px solid var(--transparent-dark);
+}
+
+.item-text-meta {
+  display: flex;
+  font-size: 0.7em;
+}
+
+.item-text-meta > span {
+  margin: auto 10px;
+  width: auto;
+}
+
+.cart-items {
+  height: 60vh;
+  overflow: hidden;
+}
+
+.item-image {
+  width: 15px;
+  height: 40px;
+}
+
+.invoice-section {
+  border-top: 1px solid var(--off-black);
+  padding-top: 1em;
+}
+
+.invoice-amount {
+    color: var(--off-black);
+    font-family: var(--iowan);
+    font-weight: 800;
+}
+</style>
---
+++ src/views/CartView.vue
@@ -0,0 +1,58 @@
+<!-- eslint-disable max-len -->
+<template>
+    <div class="main-container">
+        <Navbar />
+
+        <div class="main-container-content">
+            <DeliveryColumn />
+            <CartColumn />
+        </div>
+
+        <Footer />
+    </div>
+</template>
+
+<script>
+import Navbar from '@/components/UI/navbar-component.vue';
+import Footer from '@/components/UI/footer-component.vue';
+import DeliveryColumn from '@/components/Cart/delivery-form.vue';
+import CartColumn from '@/components/Cart/cart-contents.vue';
+
+export default {
+    name: 'CartView',
+    components: {
+        Navbar,
+        Footer,
+        DeliveryColumn,
+        CartColumn,
+    },
+
+    data() {
+        return {
+            data() {
+                return {
+                    testValue: 'testy',
+                };
+            },
+        };
+    },
+};
+</script>
+
+<style scoped>
+.main-container {
+  width: 80vw;
+  margin: 0 auto;
+}
+
+.main-container-content {
+  display: flex;
+  min-height: 85vh;
+}
+
+@media (max-width: 700px) {
+  .main-container-content {
+      flex-direction: column-reverse;
+  }
+  }
+</style>
